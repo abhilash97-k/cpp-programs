7c73d0800b9101e004c64382d6733be03a1b5ef4
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-09-04 19:54:46 +0530

    String length using for loop
    Finding the string length using for loop

---
+++ nostrlen.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char st[8];
+    int i;
+    cout<<"Enter the name";
+    cin>>st;
+    for(i=0;st[i]!='\0';i++ )
+    {
+        i = i+1;
+    }
+    cout<<i;
+
+    return 0;
+}
