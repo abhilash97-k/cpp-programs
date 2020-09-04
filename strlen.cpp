f77c3af74377d562954e66df88b82ff3ffd72e88
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-09-04 20:01:04 +0530

    String length using strlen function

---
+++ strlen.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+ int main()
+ {
+     char st[8];
+     cout<<"Enter the name";
+     cin>>st;
+     int len = strlen(st);
+     cout<<len;
+     return 0;
+ }
