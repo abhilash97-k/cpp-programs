e45c03b14195bbad69c438f0af1fc39288322f54
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-09-07 14:44:41 +0530

    By giving input number (1 - 12) print the month.

---
+++ month.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    long m;
+    cout<<"Enter the number of month"<<endl;
+    cin>>m;
+    switch(m)
+    {
+    case 1:
+        cout<<"Jan";
+        break;
+    case 2:
+        cout<<"Feb";
+        break;
+    case 3:
+        cout<<"March";
+        break;
+    case 4:
+        cout<<"April";
+        break;
+    case 5:
+        cout<<"May";
+        break;
+    case 6:
+        cout<<"June";
+        break;
+    case 7:
+        cout<<"July";
+        break;
+    case 8:
+        cout<<"Aug";
+        break;
+    case 9:
+        cout<<"Sep";
+        break;
+    case 10:
+        cout<<"Oct";
+        break;
+    case 11:
+        cout<<"Nov";
+        break;
+    case 12:
+        cout<<"Dec";
+        break;
+    }
+    return 0;
+}
