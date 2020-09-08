7a1326e33a90dca14fada838d83fe985a31ea1aa
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-09-08 18:04:20 +0530

    Given input in ascending order or descending order

---
+++ 8_sep_20cpp/AscDesending.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int p , q;
+    cout<<"Enter the two numbers"<<endl;
+    cin>>p>>q;
+    if(p>q)
+    {
+        cout<<"This is in Descending";
+    }
+    else
+    {
+        cout<<"This is ascending";
+    }
+    return 0;
+}
