90a201461d5b0597f4dc29e927bde9673b56aa57
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-09-08 18:14:25 +0530

    For loop function without return type and no arguments

---
+++ 8_sep_20cpp/forLoopFun.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void forLoop()
+{
+    int n, i;
+    cin>>n;
+    for( i = 0;i<=n;i++)
+    {
+        cout<<i<<endl;
+    }
+}
+
+int main()
+{
+    forLoop();
+    return 0;
+}
