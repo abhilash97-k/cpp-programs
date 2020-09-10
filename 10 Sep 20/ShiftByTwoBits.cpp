1f0844c8a1c8a41bf502fb253a15e4c13eccd835
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-09-10 16:41:56 +0530

    Shift the data by 2 bits two the left

---
+++ 10 Sep 20/ShiftByTwoBits.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the Data";
+    cin>>a;
+    a<<=2;
+    cout<<"Data after shifted two bits"<<endl;
+    cout<<a;
+    return 0;
+}
