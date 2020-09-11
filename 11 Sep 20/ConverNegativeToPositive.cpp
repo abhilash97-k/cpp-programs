a5467543b4cdd061271c07f9497700359c8a5ac2
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-09-11 18:16:42 +0530

    Convert any input number into positive number

---
+++ 11 Sep 20/ConverNegativeToPositive.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter the number";
+    cin>>num;
+    if(num<0)
+    {
+        num = num * (-1);
+    }
+    else
+        num;
+    cout<<num;
+}
